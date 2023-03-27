e8a0bd85adeaca9d1eab7c980445b65419913b5d
= <=>
2023-03-27 14:44:41 +0800

    Completed the left navigation.

--- src/webparts/leftNavigation/LeftNavigationWebPart.ts
+++ src/webparts/leftNavigation/LeftNavigationWebPart.ts
@@ -2,119 +2,123 @@ import * as React from 'react';
 import * as ReactDom from 'react-dom';
 import { Version } from '@microsoft/sp-core-library';
 import {
-  IPropertyPaneConfiguration,
-  PropertyPaneTextField
+	IPropertyPaneConfiguration,
+	PropertyPaneTextField
 } from '@microsoft/sp-property-pane';
 import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
 import { IReadonlyTheme } from '@microsoft/sp-component-base';
-
+import { SPFI, spfi, SPFx } from "@pnp/sp";
 import * as strings from 'LeftNavigationWebPartStrings';
 import LeftNavigation from './components/LeftNavigation';
-import { ILeftNavigationProps } from './components/ILeftNavigationProps';
+import { ILeftNavigationProps } from './components/LeftNavigation';
 
 export interface ILeftNavigationWebPartProps {
-  description: string;
+	description: string;
 }
 
 export default class LeftNavigationWebPart extends BaseClientSideWebPart<ILeftNavigationWebPartProps> {
 
-  private _isDarkTheme: boolean = false;
-  private _environmentMessage: string = '';
+	private _isDarkTheme: boolean = false;
+	private _environmentMessage: string = '';
+	private _sp: SPFI;
 
-  public render(): void {
-    const element: React.ReactElement<ILeftNavigationProps> = React.createElement(
-      LeftNavigation,
-      {
-        description: this.properties.description,
-        isDarkTheme: this._isDarkTheme,
-        environmentMessage: this._environmentMessage,
-        hasTeamsContext: !!this.context.sdks.microsoftTeams,
-        userDisplayName: this.context.pageContext.user.displayName
-      }
-    );
+	public render(): void {
+		const element: React.ReactElement<ILeftNavigationProps> = React.createElement(
+			LeftNavigation,
+			{
+				description: this.properties.description,
+				isDarkTheme: this._isDarkTheme,
+				environmentMessage: this._environmentMessage,
+				hasTeamsContext: !!this.context.sdks.microsoftTeams,
+				userDisplayName: this.context.pageContext.user.displayName,
+				sp: this._sp
+			}
+		);
 
-    ReactDom.render(element, this.domElement);
-  }
+		ReactDom.render(element, this.domElement);
+	}
 
-  protected onInit(): Promise<void> {
-    return this._getEnvironmentMessage().then(message => {
-      this._environmentMessage = message;
-    });
-  }
+	protected async onInit(): Promise<void> {
+		await super.onInit();
+		this._sp = spfi().using(SPFx(this.context));
+		return this._getEnvironmentMessage().then(message => {
+			this._environmentMessage = message;
+		});
+	}
 
 
 
-  private _getEnvironmentMessage(): Promise<string> {
-    if (!!this.context.sdks.microsoftTeams) { // running in Teams, office.com or Outlook
-      return this.context.sdks.microsoftTeams.teamsJs.app.getContext()
-        .then(context => {
-          let environmentMessage: string = '';
-          switch (context.app.host.name) {
-            case 'Office': // running in Office
-              environmentMessage = this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentOffice : strings.AppOfficeEnvironment;
-              break;
-            case 'Outlook': // running in Outlook
-              environmentMessage = this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentOutlook : strings.AppOutlookEnvironment;
-              break;
-            case 'Teams': // running in Teams
-              environmentMessage = this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentTeams : strings.AppTeamsTabEnvironment;
-              break;
-            default:
-              throw new Error('Unknown host');
-          }
+	private _getEnvironmentMessage(): Promise<string> {
+		if (!!this.context.sdks.microsoftTeams) { // running in Teams, office.com or Outlook
+			return this.context.sdks.microsoftTeams.teamsJs.app.getContext()
+				.then(context => {
+					let environmentMessage: string = '';
+					switch (context.app.host.name) {
+						case 'Office': // running in Office
+							environmentMessage = this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentOffice : strings.AppOfficeEnvironment;
+							break;
+						case 'Outlook': // running in Outlook
+							environmentMessage = this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentOutlook : strings.AppOutlookEnvironment;
+							break;
+						case 'Teams': // running in Teams
+							environmentMessage = this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentTeams : strings.AppTeamsTabEnvironment;
+							break;
+						default:
+							throw new Error('Unknown host');
+					}
 
-          return environmentMessage;
-        });
-    }
+					return environmentMessage;
+				});
+		}
 
-    return Promise.resolve(this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentSharePoint : strings.AppSharePointEnvironment);
-  }
+		return Promise.resolve(this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentSharePoint : strings.AppSharePointEnvironment);
+	}
 
-  protected onThemeChanged(currentTheme: IReadonlyTheme | undefined): void {
-    if (!currentTheme) {
-      return;
-    }
+	protected onThemeChanged(currentTheme: IReadonlyTheme | undefined): void {
+		if (!currentTheme) {
+			return;
+		}
 
-    this._isDarkTheme = !!currentTheme.isInverted;
-    const {
-      semanticColors
-    } = currentTheme;
+		this._isDarkTheme = !!currentTheme.isInverted;
+		const {
+			semanticColors
+		} = currentTheme;
 
-    if (semanticColors) {
-      this.domElement.style.setProperty('--bodyText', semanticColors.bodyText || null);
-      this.domElement.style.setProperty('--link', semanticColors.link || null);
-      this.domElement.style.setProperty('--linkHovered', semanticColors.linkHovered || null);
-    }
+		if (semanticColors) {
+			this.domElement.style.setProperty('--bodyText', semanticColors.bodyText || null);
+			this.domElement.style.setProperty('--link', semanticColors.link || null);
+			this.domElement.style.setProperty('--linkHovered', semanticColors.linkHovered || null);
+		}
 
-  }
+	}
 
-  protected onDispose(): void {
-    ReactDom.unmountComponentAtNode(this.domElement);
-  }
+	protected onDispose(): void {
+		ReactDom.unmountComponentAtNode(this.domElement);
+	}
 
-  protected get dataVersion(): Version {
-    return Version.parse('1.0');
-  }
+	protected get dataVersion(): Version {
+		return Version.parse('1.0');
+	}
 
-  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
-    return {
-      pages: [
-        {
-          header: {
-            description: strings.PropertyPaneDescription
-          },
-          groups: [
-            {
-              groupName: strings.BasicGroupName,
-              groupFields: [
-                PropertyPaneTextField('description', {
-                  label: strings.DescriptionFieldLabel
-                })
-              ]
-            }
-          ]
-        }
-      ]
-    };
-  }
+	protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+		return {
+			pages: [
+				{
+					header: {
+						description: strings.PropertyPaneDescription
+					},
+					groups: [
+						{
+							groupName: strings.BasicGroupName,
+							groupFields: [
+								PropertyPaneTextField('description', {
+									label: strings.DescriptionFieldLabel
+								})
+							]
+						}
+					]
+				}
+			]
+		};
+	}
 }
--- src/webparts/leftNavigation/components/LeftNavigation.tsx
+++ src/webparts/leftNavigation/components/LeftNavigation.tsx
@@ -1,43 +1,115 @@
 import * as React from 'react';
+import { FC, useState, useEffect } from 'react';
 import styles from './LeftNavigation.module.scss';
-import { ILeftNavigationProps } from './ILeftNavigationProps';
-import { escape } from '@microsoft/sp-lodash-subset';
+import { SPFI } from '@pnp/sp';
+import "@pnp/sp/webs";
+import "@pnp/sp/site-users/web";
+import "@pnp/sp/lists/web";
+import "@pnp/sp/items";
+import { INavLink, INavLinkGroup, Nav, Spinner, Stack } from 'office-ui-fabric-react';
 
-export default class LeftNavigation extends React.Component<ILeftNavigationProps, {}> {
-  public render(): React.ReactElement<ILeftNavigationProps> {
-    const {
-      description,
-      isDarkTheme,
-      environmentMessage,
-      hasTeamsContext,
-      userDisplayName
-    } = this.props;
-
-    return (
-      <section className={`${styles.leftNavigation} ${hasTeamsContext ? styles.teams : ''}`}>
-        <div className={styles.welcome}>
-          <img alt="" src={isDarkTheme ? require('../assets/welcome-dark.png') : require('../assets/welcome-light.png')} className={styles.welcomeImage} />
-          <h2>Well done, {escape(userDisplayName)}!</h2>
-          <div>{environmentMessage}</div>
-          <div>Web part property value: <strong>{escape(description)}</strong></div>
-        </div>
-        <div>
-          <h3>Welcome to SharePoint Framework!</h3>
-          <p>
-            The SharePoint Framework (SPFx) is a extensibility model for Microsoft Viva, Microsoft Teams and SharePoint. It&#39;s the easiest way to extend Microsoft 365 with automatic Single Sign On, automatic hosting and industry standard tooling.
-          </p>
-          <h4>Learn more about SPFx development:</h4>
-          <ul className={styles.links}>
-            <li><a href="https://aka.ms/spfx" target="_blank" rel="noreferrer">SharePoint Framework Overview</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-graph" target="_blank" rel="noreferrer">Use Microsoft Graph in your solution</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-teams" target="_blank" rel="noreferrer">Build for Microsoft Teams using SharePoint Framework</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-viva" target="_blank" rel="noreferrer">Build for Microsoft Viva Connections using SharePoint Framework</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-store" target="_blank" rel="noreferrer">Publish SharePoint Framework applications to the marketplace</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-api" target="_blank" rel="noreferrer">SharePoint Framework API reference</a></li>
-            <li><a href="https://aka.ms/m365pnp" target="_blank" rel="noreferrer">Microsoft 365 Developer Community</a></li>
-          </ul>
-        </div>
-      </section>
-    );
-  }
+export interface ILeftNavigationProps {
+	description: string;
+	isDarkTheme: boolean;
+	environmentMessage: string;
+	hasTeamsContext: boolean;
+	userDisplayName: string;
+	sp: SPFI;
 }
+
+const LeftNavigation: FC<ILeftNavigationProps> = (props) => {
+	const { sp } = props;
+	const [loading, setLoading] = useState<boolean>(true);
+	const [menuItems, setMenuItems] = useState<INavLinkGroup[]>(undefined);
+	const [selMenu, setSelMenu] = useState<string>(undefined);
+
+	const getActiveMenuItems = async (): Promise<any[]> => {
+		const filQuery = `IsActive eq 1 and Position eq 'Left'`;
+		return await sp.web.lists.getByTitle('Menus').items
+			.select('ID', 'Title', 'PageUrl', 'IconName', 'Sequence', 'IsParent', 'ParentMenu/Id', 'ParentMenu/Title', 'IsActive')
+			.expand('ParentMenu')
+			.filter(filQuery)();
+	};
+
+	const _loadLeftNavigation = async (): Promise<void> => {
+		const menuItems: any[] = await getActiveMenuItems();
+		if (menuItems.length > 0) {
+			const navLinks: INavLinkGroup[] = [];
+			const navLink: INavLink[] = [];
+			if (menuItems.length > 0) {
+				const fil = menuItems.filter((mi: any) => mi.IsParent);
+				if (fil && fil.length > 0) {
+					fil.map((item: any) => {
+						const subMenus: any[] = menuItems.filter((smi: any) => !smi.IsParent && smi.ParentMenu?.Id === item.ID);
+						let navsubLink: INavLink[] = [];
+						if (subMenus && subMenus.length > 0) {
+							subMenus.map((item: any) => {
+								if (item.PageUrl?.Url.toLowerCase() === (window.location.origin + window.location.pathname).toLowerCase())
+									setSelMenu(item.ID.toString());
+								navsubLink.push({
+									key: item.ID.toString(),
+									name: item.Title,
+									url: item.PageUrl?.Url,
+									expandAriaLabel: item.Title,
+									icon: item.IconName,
+								});
+							});
+						}
+						if (item.PageUrl?.Url.toLowerCase() === (window.location.origin + window.location.pathname).toLowerCase())
+							setSelMenu(item.ID.toString());
+						navLink.push({
+							key: item.ID.toString(),
+							name: item.Title,
+							url: item.PageUrl?.Url,
+							expandAriaLabel: item.Title,
+							icon: item.IconName,
+							links: navsubLink.length > 0 ? navsubLink : [],
+							isExpanded: true
+						});
+						navsubLink = [];
+					});
+				}
+				navLinks.push({ links: navLink });
+				setMenuItems(navLinks);
+			}
+		}
+		setLoading(false);
+	};
+
+	const _linkClick = (ev?: React.MouseEvent<HTMLElement, MouseEvent>, item?: INavLink): void => {
+		if (item) {
+			setSelMenu(item.ID.toString());
+		}
+	};
+
+	useEffect(() => {
+		(async () => {
+			await _loadLeftNavigation();
+		})();
+	}, []);
+
+	return (
+		<div className={styles.leftNavigation}>
+			<Stack tokens={{ childrenGap: 10 }} horizontal horizontalAlign='start'>
+				<Stack.Item style={{ width: '25%', borderRight: '1px solid #CCC' }}>
+					<div style={{ marginRight: '5px' }}>
+						{loading ? (
+							<Spinner label='Please wait...' labelPosition='top' />
+						) : (
+							<>
+								<Nav selectedKey={selMenu} groups={menuItems} className={styles.leftNavigation} onLinkClick={_linkClick} />
+							</>
+						)}
+					</div>
+				</Stack.Item>
+				<Stack.Item style={{width: '75%'}}>
+					<div>
+						Page Content goes here.
+					</div>
+				</Stack.Item>
+			</Stack>
+		</div>
+	);
+};
+
+export default LeftNavigation;
