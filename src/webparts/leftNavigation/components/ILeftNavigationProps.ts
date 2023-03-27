0105b0dcc000b368de8506da1492a8cd8f55fbdb
SP Admin <spadmin@NTT-D4DG593.localdomain>
2023-03-27 12:59:30 +0800

    Initial Commit

---
+++ src/webparts/leftNavigation/components/ILeftNavigationProps.ts
@@ -0,0 +1,7 @@
+export interface ILeftNavigationProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/leftNavigation/loc/mystrings.d.ts
@@ -0,0 +1,18 @@
+declare interface ILeftNavigationWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+}
+
+declare module 'LeftNavigationWebPartStrings' {
+  const strings: ILeftNavigationWebPartStrings;
+  export = strings;
+}
